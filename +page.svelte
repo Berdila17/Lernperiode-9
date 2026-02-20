9addf8d21e1be50281137b66b9ec186fd07f124f
Berdila17 <l.berdila.inf24@stud.bbbaden.ch>
2026-02-20 12:01:34 +0100

    Create +page.svelte

---
+++ +page.svelte
@@ -0,0 +1,27 @@
+<script>
+  // JS
+  let name = '';
+</script>
+
+<!-- HTML -->
+<h1>Tracer Bullet</h1>
+
+<input bind:value={name} placeholder="Laurentiu" />
+
+<p>Hello {name}</p>
+
+<style>
+  /* CSS */
+  h1 {
+    color: darkblue;
+  }
+
+  input {
+    padding: 6px;
+    margin-top: 10px;
+  }
+
+  p {
+    font-weight: bold;
+  }
+</style>
